c936343b558b38f584861d2a8f6ac4c5157c5017
fauzaan97 <muhammadaqiilfauzaan2004@mail.ugm.ac.id>
2024-09-09 10:17:06 +0700

    Class Diagram Implementation

---
+++ Treashure/Plastic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treashure
+{
+    public class Plastic
+    {
+        public int MaterialID { get; set; }
+        public string Type { get; set; }
+        public float Quantity { get; set; }
+
+        public void AddTrash(float amount)
+        {
+            Quantity += amount;
+        }
+
+        public string GetTrashInfo()
+        {
+            return $"Material ID: {MaterialID}, Type: {Type}, Quantity: {Quantity} kg";
+        }
+
+        public void UpdateTrash(float newQuantity)
+        {
+            Quantity = newQuantity;
+        }
+    }
+
+}
---
+++ Treashure/admin.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treashure
+{
+    public class Admin : User
+    {
+        public int AdminID { get; set; }
+        public bool Permission { get; set; }
+
+        public void ManageUserHistory(User user)
+        {
+            // Logic to manage user's history
+            foreach (var transaction in user.CheckHistory())
+            {
+                // Display or process the user's transaction history
+            }
+        }
+
+        public void ManageATM(PlasticATM atm, string newStatus)
+        {
+            atm.UpdateStatus(newStatus);
+        }
+
+        public void ManageTrashData(Plastic trash)
+        {
+            // Logic to manage plastic data
+        }
+    }
+
+}
