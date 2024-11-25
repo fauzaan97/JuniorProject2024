1f9c12235a68e8d69f740735be271b940822c05b
Timotius2005 <timotiuskristafaelharjanto2005@mail.ugm.ac.id>
2024-11-25 12:04:59 +0700

    Add admin ui

--- Treashure/Form3.Designer.cs
+++ Treashure/Form3.Designer.cs
@@ -36,6 +36,17 @@
             pictureBox1 = new PictureBox();
             splitter1 = new Splitter();
             label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton5 = new RadioButton();
+            label5 = new Label();
+            textBox2 = new TextBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
@@ -125,9 +136,126 @@
             label1.ForeColor = SystemColors.ButtonHighlight;
             label1.Location = new Point(274, 9);
             label1.Name = "label1";
-            label1.Size = new Size(273, 26);
+            label1.Size = new Size(237, 26);
             label1.TabIndex = 14;
-            label1.Text = "TRASH AND TPS STATS";
+            label1.Text = "WELCOME ADMIN!";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(226, 47);
+            label2.Name = "label2";
+            label2.Size = new Size(327, 26);
+            label2.TabIndex = 16;
+            label2.Text = "INSERT NEW COLLECTION";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(97, 110);
+            label3.Name = "label3";
+            label3.Size = new Size(217, 26);
+            label3.TabIndex = 17;
+            label3.Text = "Collector Username";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(331, 105);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(302, 31);
+            textBox1.TabIndex = 18;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(178, 151);
+            label4.Name = "label4";
+            label4.Size = new Size(136, 26);
+            label4.TabIndex = 19;
+            label4.Text = "Plastic Type";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(331, 151);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(188, 29);
+            radioButton1.TabIndex = 20;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "PET (Plastic Bottles)";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(331, 186);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(184, 29);
+            radioButton2.TabIndex = 21;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "LDPE (Plastic Bags)";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(331, 221);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(203, 29);
+            radioButton3.TabIndex = 22;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "PP (Food Containers)";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(331, 256);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(231, 29);
+            radioButton4.TabIndex = 23;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "Cleaning Product Bottles";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(331, 291);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(189, 29);
+            radioButton5.TabIndex = 24;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "Single-User Plastics";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ButtonHighlight;
+            label5.Location = new Point(156, 344);
+            label5.Name = "label5";
+            label5.Size = new Size(158, 26);
+            label5.TabIndex = 25;
+            label5.Text = "Enter Amount";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(331, 344);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(302, 31);
+            textBox2.TabIndex = 26;
             // 
             // Form3
             // 
@@ -135,6 +263,17 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.TPS_Manager;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
+            Controls.Add(label5);
+            Controls.Add(radioButton5);
+            Controls.Add(radioButton4);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label4);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(pictureBox6);
             Controls.Add(pictureBox5);
@@ -166,5 +305,16 @@
         private PictureBox pictureBox1;
         private Splitter splitter1;
         private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label4;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private RadioButton radioButton5;
+        private Label label5;
+        private TextBox textBox2;
     }
 }
